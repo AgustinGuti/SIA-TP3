26aa84ac793876f5d5f9627653ae6b49342873d5
Agus <agutierrez@itba.edu.ar>
2024-05-02 09:28:43 -0300

    Initial adam implementation
    Co-authored-by: LUCAS BIOLLEY <DJ4S@users.noreply.github.com>
    Co-authored-by: Jeremias Feferovich <JeremiasFeferovich@users.noreply.github.com>

--- ej3_c.py
+++ ej3_c.py
@@ -64,16 +64,19 @@ class NeuralNetwork:
                     training_input = data_input[mu]
                 next_layer_deltas, next_layer_weights = layer.train(training_input, next_layer_deltas, next_layer_weights)
 
-            error = np.sum((expected_output - results[-1])**2)/2
+                
+            error = sum(sum((expected_output[mu] - self.process(data_input[mu]))**2 for mu in range(0, len(expected_output)))/2)
 
+            improved = ''
             if error < self.min_error:
                 error_history.append(error)
                 best_weights_history.append(self.get_weights())
                 self.min_error = error
                 for layer in self.layers:
                     layer.set_min_weights()
+                improved = '- True'
 
-            print(f'Iteration: {iteration} - Error: {error} - Min Error: {self.min_error}')
+            print(f'Iteration: {iteration} - Error: {error} - Min Error: {self.min_error} {improved}')
 
             iteration += 1
         return self.min_error, iteration, best_weights_history, error_history
@@ -89,7 +92,8 @@ class Layer:
             neuron.min_bias = neuron.bias
 
     def process(self, data_input, best=False):
-        return np.array([neuron.predict(data_input, best) for neuron in self.neurons])
+        arr = np.array([neuron.predict(data_input, best) for neuron in self.neurons])
+        return arr
 
     def train_last_layer(self, expected_output, last_layer_output):
         layer_deltas = []
@@ -111,7 +115,50 @@ class Layer:
             layer_deltas.append(neuron_delta)
             layer_weights.append(neuron_weights)
         return layer_deltas, layer_weights
+
+class MomentumParams:
+    def __init__(self, beta=0.9):
+        self.beta = beta
+        self.last_m = 0
+
+    def get_m(self, iteration, derivatives):
+        m = self.beta * self.last_m + (1 - self.beta) * derivatives
+        self.last_m = m
+        return m / (1 - self.beta**iteration)
+    
+class RMSPropParams:
+    def __init__(self, beta=0.9):
+        self.beta = beta
+        self.last_v = 0
+
+    def get_v(self, iteration, gradient):
+        v = self.beta * self.last_v + (1 - self.beta) * gradient**2
+        self.last_v = v
+        return v / (1 - self.beta**iteration)
+    
+    def get_delta(self, iteration, gradient):
+        return gradient / np.sqrt(self.get_v(iteration, gradient) + 1e-8)
+
+class AdamParams:
+    def __init__(self, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.momentum_params = MomentumParams(beta1)
+        self.rms_prop_params = RMSPropParams(beta2)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+
+    def _get_m(self, iteration, derivatives):
+        return self.momentum_params.get_m(iteration, derivatives)
+
+    def _get_v(self, iteration, derivatives):
+        return self.rms_prop_params.get_v(iteration, derivatives)
         
+    def get_delta(self, iteration, derivatives):
+        m = self._get_m(iteration, derivatives)
+        v = self._get_v(iteration, derivatives)
+        return m / (np.sqrt(v + self.epsilon)) # TODO check if it should be v or v + epsilon
+    
+
 class Neuron:
     def __init__(self, params: NeuronParams, id, layer_id=None):
         self.weights = np.random.rand(params.dimensions)
@@ -123,6 +170,11 @@ class Neuron:
         self.learning_rate = params.learning_rate
         self.activation_function = params.activation_function
         self.beta = params.beta
+        self.adam_params = AdamParams()
+        self.momentum_params = MomentumParams()
+        self.rms_prop_params = RMSPropParams()
+        self.iteration = 1
+        self.optimizer = 'a'
 
     activation_functions = {
         'linear': lambda x, b=0: x,
@@ -151,39 +203,73 @@ class Neuron:
         excitement = self.compute_excitement(data_input)
         next_layer_weights = np.array(next_layer_weights)
         aux = np.sum(np.multiply(next_layer_deltas, next_layer_weights[:, self.id]))
+
         derivative = self.derivative_activation_functions[self.activation_function](excitement, self.beta)
-        delta = aux * derivative
-        return self.learning_rate * delta, delta
+
+        error_gradient = np.multiply(aux * derivative, data_input)
+
+        # Shoult it be aux?
+        if self.optimizer == 'adam':
+            delta = self.adam_params.get_delta(self.iteration, error_gradient)
+        elif self.optimizer == 'momentum':
+            delta = self.momentum_params.get_m(self.iteration, error_gradient)
+        elif self.optimizer == 'rms':
+            delta = self.rms_prop_params.get_delta(self.iteration, error_gradient)
+        else:
+            delta = aux * derivative
+
+        # print(f'Iteration: {self.iteration} - Delta: {delta} - Error Gradient: {error_gradient} - Layer: {self.layer_id}')
+        return np.multiply(self.learning_rate, delta), aux*derivative
     
-    def train_last_layer(self, expected_output, last_layer_output):
-        excitement = self.compute_excitement(last_layer_output)
+    def train_last_layer(self, expected_output, previous_layer_output):
+        excitement = self.compute_excitement(previous_layer_output)
         activation = self.compute_activation(excitement)
+
         derivative = self.derivative_activation_functions[self.activation_function](excitement, self.beta)
-        delta = (expected_output - activation) * derivative
 
-        delta_weights = np.multiply(self.learning_rate * delta, last_layer_output)
+        error_gradient = -np.multiply((expected_output - activation) * derivative, previous_layer_output)
 
-        self.weights = self.weights + delta_weights
-        self.bias = self.bias + self.learning_rate * delta
+        if self.optimizer == 'adam':
+            delta = self.adam_params.get_delta(self.iteration, error_gradient)
+            self.weights = self.weights + self.learning_rate * delta  
+        elif self.optimizer == 'momentum':
+            delta = self.momentum_params.get_m(self.iteration, error_gradient)
+            self.weights = self.weights + delta * self.learning_rate
+        elif self.optimizer == 'rms':
+            delta = self.rms_prop_params.get_delta(self.iteration, error_gradient)
+            self.weights = self.weights + delta * self.learning_rate
+        else:
+            delta = (expected_output - activation) * derivative
+            delta_weights = np.multiply(self.learning_rate * delta, previous_layer_output)
+            self.weights = self.weights + delta_weights
+
+        self.bias = self.bias + self.learning_rate * (expected_output - activation) * derivative
+        self.iteration += 1
         return delta, self.weights
 
     def train(self, data_input, next_layer_delta, next_layer_weights):
         delta_weight, delta = self.calculate_delta_weights(data_input, next_layer_delta, next_layer_weights)
-        delta_weights = []
 
-        for i in range(len(self.weights)):
-            delta_weights.append(delta_weight * data_input[i])
+        if self.optimizer == 'adam' or self.optimizer == 'momentum' or self.optimizer == 'rms':
+            self.weights = self.weights + delta_weight 
+        else:
+            delta_weights = []
 
-        self.weights = self.weights + delta_weights
-        self.bias = self.bias + delta_weight
+            for i in range(len(self.weights)):
+                delta_weights.append(delta_weight * data_input[i])
+            self.weights = self.weights + delta_weights
+
+        self.bias = self.bias + delta * self.learning_rate
+        self.iteration += 1
         return delta, self.weights
 
 def main():
     with open('TP3-ej3-digitos.txt', 'r') as f:
         lines = [line.split() for line in f]
     data = [list(map(int, [num for sublist in lines[i:i+7] for num in sublist])) for i in range(0, len(lines), 7)]
-    
+
     example_data_input = data
+
     # example_data_input = np.insert(example_data_input, 0, 1, axis=0)
     example_data_output = np.array([
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -199,28 +285,60 @@ def main():
     ])
 
     # example_data_output = np.array([[i] for i in range(10)])
+    
+    # example_data_input = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+    # example_data_output = np.array([[-1], [-1], [-1], [1]])
+
+    # example_data_input = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+    # example_data_output = np.array([[1], [1], [-1], [-1]])
 
     dimensions = len(example_data_input[0])
-    layer_one = [NeuronParams(dimensions, 5, 'linear', 0.5) for _ in range(100)]
-    layer_two = [NeuronParams(len(layer_one),5,  'tan_h', 0.1) for _ in range(50)]
-    layer_three = [NeuronParams(len(layer_two), 50, 'sigmoid', 0.2) for _ in range(10)]
-    # layer_four = [NeuronParams(len(layer_three), 0.05, 'linear', 0.5) for _ in range(1)]
+    layer_one = [NeuronParams(dimensions, 0.01, 'linear', 0.2) for _ in range(50)]
+    layer_two = [NeuronParams(len(layer_one), 0.01,  'tan_h', 0.2) for _ in range(5)]
+    layer_three = [NeuronParams(len(layer_two), 0.01, 'tan_h', 0.5) for _ in range(10)]
+    # layer_four = [NeuronParams(len(layer_three), 0.001, 'linear', 0.5) for _ in range(1)]
 
-    # layer_four = [NeuronParams(len(layer_three), 10, 'softmax', 0.05) for _ in range(10)]
+    # example_data_input = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
+    # example_data_output = np.array([[1], [1], [-1], [-1]])
+
+    # dimensions = len(example_data_input[0])
+    # layer_one = [NeuronParams(dimensions, 1, 'linear', 0.1) for _ in range(1)]
+    # layer_two = [NeuronParams(len(layer_one), 1, 'tan_h', 0.2) for _ in range(5)]
+    # layer_three = [NeuronParams(len(layer_two), 1, 'tan_h', 0.5) for _ in range(1)]
 
     neurons_params = [layer_one, layer_two, layer_three]
 
-    neural_network = NeuralNetwork(neurons_params, 300)
+    neural_network = NeuralNetwork(neurons_params, 1000)
+
     min_error, iterations, best_weights_history, error_history = neural_network.train(example_data_input, example_data_output)
     # neural_network.print_weights()
     print(f'Min error: {min_error} - Iterations: {iterations}')
 
-
+    results = []
     for i, input_data in enumerate(example_data_input):
         result = neural_network.predict(input_data)
         result = [round(res, 2) for res in result]
         print(f'Result {i}: {result} - expected: {example_data_output[i]}')
+        results.append(result)
 
+#     fig, axs = plt.subplots(len(results), figsize=(10, 30))
+
+# # For each input, create a bar chart of the result and expected result
+#     for i, (result, expected) in enumerate(zip(results, example_data_output)):
+#         axs[i].bar(np.arange(len(result)), result, alpha=0.7, label='Result')
+#         axs[i].bar(np.arange(len(expected)), expected, alpha=0.7, label='Expected')
+#         axs[i].set_title(f'Input {i+1}')
+#         axs[i].legend()
+
+#     plt.tight_layout()
+#     plt.show()
+
+    # for weights in best_weights_history:
+    #     neural_network.set_min_weights(weights)
+    #     for i, input_data in enumerate(example_data_input):
+    #         result = neural_network.predict(input_data)
+    #         result = [round(res, 2) for res in result]
+    #         print(f'Result {i}: {result} - expected: {example_data_output[i]}')
 
 if __name__ == "__main__":
     main()

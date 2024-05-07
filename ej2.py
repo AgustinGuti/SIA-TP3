ed1efd521f1f0c68690f0057e8fa175d88b07e1e
Agus <agutierrez@itba.edu.ar>
2024-05-07 19:31:40 -0300

    Fix data scales

--- ej2.py
+++ ej2.py
@@ -3,8 +3,6 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import MinMaxScaler
 
 class Perceptron:
     def __init__(self, dimensions, learning_rate=0.1, activation_function='linear', beta=100):
@@ -30,14 +28,19 @@ class Perceptron:
         'tan_h': lambda x, b: b*(1 - np.tanh(b*x)**2)
     }        
 
-    def compute_excitement(self, value):
-        return sum(value * self.weights) + self.bias
+    def compute_excitement(self, value, best=False):
+        weights = self.weights if not best else self.min_weights
+        bias = self.bias if not best else self.min_bias
+        return sum(value * weights) + bias
 
     def compute_activation(self, excitement):
         return self.activation_functions[self.activation_function](excitement, self.beta)
     
+    def process(self, data_input):
+        return [self.compute_activation(self.compute_excitement(value, False)) for value in data_input]
+
     def predict(self, data_input):
-        return [self.compute_activation(self.compute_excitement(value)) for value in data_input]
+        return [self.compute_activation(self.compute_excitement(value, True)) for value in data_input]
     
     def calculate_delta(self, value, expected_output):
         excitement = self.compute_excitement(value)
@@ -65,7 +68,8 @@ class Perceptron:
             self.bias = self.bias + base_delta
             self.weights = self.weights + base_delta * value
 
-            error = sum((expected_output[mu] - self.compute_activation(self.compute_excitement(data_input[mu])))**2 for mu in range(0, len(data_input)))/len(data_input)
+            predictions = self.process(data_input)
+            error = calculate_error(predictions, expected_output)
       
             if error < self.min_error:
                 weight_history.append(self.weights)
@@ -81,7 +85,30 @@ class Perceptron:
         return self.min_weights, self.min_bias, self.min_error, weight_history, error_history, bias_history
 
 def calculate_error(data, expected_output):
-    return sum([abs(expected_output[mu] - data[mu])**2 for mu in range(0, len(data))])/2
+    return sum(calculate_errors(data, expected_output))/len(data)
+
+def calculate_errors(data, expected_output):
+    return [abs(expected_output[mu] - data[mu])**2 for mu in range(0, len(data))]
+
+class Scaler:
+    def __init__(self, min_val, max_val, activation_function='linear'):
+        self.min_val = min_val
+        self.max_val = max_val
+        self.activation_function = activation_function
+
+    def fit_transform(self, data):
+        if self.activation_function == 'linear':
+            return data
+        elif self.activation_function == 'tan_h':
+            return (data - self.min_val) / (self.max_val - self.min_val) * 2 - 1
+        return (data - self.min_val) / (self.max_val - self.min_val)
+
+    def inverse_transform(self, data):
+        if self.activation_function == 'linear':
+            return data
+        elif self.activation_function == 'tan_h':
+            return ((np.array(data) + 1) / 2 * (self.max_val - self.min_val)) + self.min_val
+        return (np.array(data) * (self.max_val - self.min_val)) + self.min_val
 
 def main():
     data = pd.read_csv('TP3-ej2-conjunto.csv')
@@ -89,12 +116,24 @@ def main():
     example_data_input = data[[col for col in data.columns if col.startswith('x')]].values    
     example_data_output = data['y'].values
 
-    scaler = MinMaxScaler(feature_range=(-1, 1))
+    # perm = np.random.permutation(len(example_data_input))
 
-    example_data_output = scaler.fit_transform(example_data_output.reshape(-1, 1))
-    example_data_output = example_data_output.ravel()
+    # example_data_input = example_data_input[perm]
+    # example_data_output = example_data_output[perm]
 
-    training_percentage = 0.7
+
+    activation_function = 'linear'
+    beta = 0.3
+    learning_rate = 0.01
+
+    # Min-Max scaling
+    min_val = np.min(example_data_output)
+    max_val = np.max(example_data_output)
+
+    scaler = Scaler(min_val, max_val, activation_function)
+    example_data_output = scaler.fit_transform(example_data_output)
+
+    training_percentage = 0.8
     cutoff = int(len(example_data_input) * training_percentage)
 
     training_data = example_data_input[:cutoff]
@@ -106,50 +145,73 @@ def main():
     min_errors = []
     test_errors = []
 
-    # for i in range(20):
-    #     min_error_acum = 0
-    #     test_error_acum = 0
-    #     for _ in range(2):
-    #         perceptron = Perceptron(5000, len(training_data[0]), 0.01, activation_function='tan_h', beta=i/10)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
 
-    #         weights, bias, min_error, weight_history, error_history, bias_history = perceptron.train(training_data, training_output)
+    # sizes = (test_output - test_output.min()) / (test_output.max() - test_output.min()) * 100
 
-    #         predictions = perceptron.predict(test_data)
-    #         test_error = calculate_error(predictions, test_output)
+    # x = test_data[:, 0]
+    # y = test_data[:, 1]
+    # z = test_data[:, 2]
+    # ax.scatter(x, y, z, s=sizes, c = test_output)
 
-    #         min_error_acum += min_error
-    #         test_error_acum += test_error
+    # ax.set_xlabel('x1')
+    # ax.set_ylabel('x2')
+    # ax.set_zlabel('x3')
 
-    #     min_errors.append(min_error_acum/10)
-    #     test_errors.append(test_error_acum/10)
+    # perceptron = Perceptron(len(training_data[0]), learning_rate, activation_function=activation_function, beta=beta)
+    # perceptron.train(training_data, training_output, 10000)
 
-    # plt.plot(range(20), min_errors, label='Training Error')
-    # plt.plot(range(20), test_errors, label='Test Error')
-    # plt.ylim(0, 1)
-    # plt.yticks(np.arange(0, 1.1, 0.1))
-    # plt.legend()
+    # predictions = np.array(perceptron.predict(test_data))
+    # test_errors = calculate_errors(predictions, test_output)
+    # # test_errors = scaler.inverse_transform(test_errors)
 
-    activation_function = 'tan_h'
-    beta = 0.3
-    learning_rate = 0.001
+    # # I want to graph errors 
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+
+    # x = test_data[:, 0]
+    # y = test_data[:, 1]
+    # z = test_data[:, 2]
+    # ax.scatter(x, y, z, s=[100]*len(test_errors),  c=test_errors, cmap='RdYlGn_r')
+
+    # ax.set_xlabel('x1')
+    # ax.set_ylabel('x2')
+    # ax.set_zlabel('x3')
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+
+    # sizes = (predictions - predictions.min()) / (predictions.max() - predictions.min()) * 100
+
+    # x = test_data[:, 0]
+    # y = test_data[:, 1]
+    # z = test_data[:, 2]
+    # ax.scatter(x, y, z, s=sizes, c=predictions)
+
+    # x = training_data[:, 0]
+    # y = training_data[:, 1]
+    # z = training_data[:, 2]
+
+    # sizes = (training_output - training_output.min()) / (training_output.max() - training_output.min()) * 100
+    # # ax.scatter(x, y, z, s=sizes, c='r')
+
+    # ax.set_xlabel('x1')
+    # ax.set_ylabel('x2')
+    # ax.set_zlabel('x3')
+
 
     perceptron = Perceptron(len(training_data[0]), learning_rate, activation_function=activation_function, beta=beta)
-    # weights, bias, min_error, weight_history, error_history, bias_history = perceptron.train(training_data, training_output)
-    # print("Weights: ", weights, "Bias: ", bias)
 
-    # predictions = perceptron.predict(test_data)
-    # test_error = calculate_error(predictions, test_output)
 
-    # print("Test error: ", test_error)
-
-    k = 7 
+    k = 7
     fold_size = int(len(training_data) / k)
     start = 0
     end = fold_size
  
     fold_errors_fold = []
     fold_errors_fold = []
-    for j in range(k):
+    for j in range(k-1):
         perceptron = Perceptron(len(training_data[0]), learning_rate, activation_function=activation_function, beta=beta)
         test_data = training_data[start:end]
         test_output = training_output[start:end]
@@ -159,12 +221,21 @@ def main():
         test_errors = []
         train_errors = []
       
-        for _ in range(150):
+        for _ in range(300):
             weights, bias, min_error, weight_history, error_history, bias_history = perceptron.train(training_data_fold, training_output_fold, 1)
+
+            train_predictions = perceptron.predict(training_data_fold)
+            local_train_predictions = scaler.inverse_transform(train_predictions)
+            local_output_fold = scaler.inverse_transform(training_output_fold)
+            train_error = calculate_error(local_train_predictions, local_output_fold)
+
             predictions = perceptron.predict(test_data)
-            test_error = calculate_error(predictions, test_output)
+            local_predictions = scaler.inverse_transform(predictions)
+            local_test_output = scaler.inverse_transform(test_output)
+            test_error = calculate_error(local_predictions, local_test_output)
+
             test_errors.append(test_error)
-            train_errors.append(min_error)
+            train_errors.append(train_error)
 
         fold_errors_fold.append(test_errors[len(test_errors) - 1])
         fold_errors_fold.append(train_errors[len(train_errors) - 1])
@@ -184,8 +255,8 @@ def main():
         end += fold_size
 
     plt.figure()
-    plt.errorbar(range(1, 8), [np.mean(errors) for errors in fold_errors_fold[1::2]], yerr=[np.std(errors) for errors in fold_errors_fold[1::2]], fmt='o-', label='Training Error')
-    plt.errorbar(range(1, 8), [np.mean(errors) for errors in fold_errors_fold[0::2]], yerr=[np.std(errors) for errors in fold_errors_fold[0::2]], fmt='o-', label='Test Error')
+    plt.errorbar(range(1, k), [np.mean(errors) for errors in fold_errors_fold[1::2]], yerr=[np.std(errors) for errors in fold_errors_fold[1::2]], fmt='o-', label='Training Error')
+    plt.errorbar(range(1, k), [np.mean(errors) for errors in fold_errors_fold[0::2]], yerr=[np.std(errors) for errors in fold_errors_fold[0::2]], fmt='o-', label='Test Error')
     plt.xlabel('Fold')
     plt.ylabel('Error')
     plt.title('Error vs Fold')
@@ -196,38 +267,11 @@ def main():
     test_percentage_errors = []
     for training_percentage in [i/10 for i in range(1, 10)]:
         cutoff = int(len(example_data_input) * training_percentage)
-        
-        # Define your lower and upper percentiles
-        lower_percentile = 5
-        upper_percentile = 100 - lower_percentile
 
         # Convert your data to numpy arrays for easier manipulation
         training_data = example_data_input[:cutoff]
         training_output = example_data_output[:cutoff]
 
-        # Calculate the IQR of the training data
-        Q1 = np.percentile(training_output, lower_percentile)
-        Q3 = np.percentile(training_output, upper_percentile)
-        IQR = Q3 - Q1
-
-        # Define the outlier step
-        outlier_step = 1.5 * IQR
-
-        # Determine a list of indices of outliers for feature
-        outlier_indices = np.where((training_output < Q1 - outlier_step) | (training_output > Q3 + outlier_step))
-        outlier_indices = np.array(outlier_indices).flatten()
-
-        # Separate the outliers
-        training_data_outliers = training_data[outlier_indices]
-        training_output_outliers = training_output[outlier_indices]
-
-        # Remove the outliers from the original training data
-        np.delete(training_data, outlier_indices)
-        np.delete(training_output, outlier_indices)
-
-        # Add the outliers to the test data
-        test_data = np.concatenate((example_data_input[cutoff:], training_data_outliers))
-        test_output = np.concatenate((example_data_output[cutoff:], training_output_outliers))
         perceptron = Perceptron(len(training_data[0]), learning_rate, activation_function=activation_function, beta=beta)
 
         test_errors = []
@@ -237,6 +281,7 @@ def main():
             train_errors.append(min_error)
             predictions = perceptron.predict(test_data)
             test_error = calculate_error(predictions, test_output)
+            test_error = scaler.inverse_transform(test_error)
             test_errors.append(test_error)
     
         training_percentaje_errors.append(train_errors[len(train_errors) - 1])
